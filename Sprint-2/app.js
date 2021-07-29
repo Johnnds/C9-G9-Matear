98183357029257ee1c4dbe4f0d6ffdc17fc05b20
Johnnds <jonnhds@gmail.com>
2021-07-29 14:06:25 -0300

    agregando estilos mobile

--- Sprint-2/app.js
+++ Sprint-2/app.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 const port = 3030;
+const path = require('path');
 
 app.use(express.static('public'));
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','index.html')));
-
-app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','productDetail.html')));
-
-app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','productCart.html')));
-
-app.get('/register',(req,res) => res.sendFile(path.join(__dirname, 'views','register.html')));
-
+app.get('/productdetail',(req,res) => res.sendFile(path.join(__dirname,'views','productDetail.html')));
+app.get('/productcart',(req,res) => res.sendFile(path.join(__dirname,'views','productCart.html')));
 app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')));
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')));
 
-
-app.listen(port,()=> console.log('Servidor corriendo en el puerto ' + port))
+app.listen(port,() => console.log('Servidor corriendo en http://localhost:' + port))
