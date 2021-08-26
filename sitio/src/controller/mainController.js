aeecdf9b684e77ec6d223f74081cb829edec2baa
LucioMart <luchoweb15@gmail.com>
2021-08-26 10:35:51 -0300

    Comenzando a hacer la lista de products.

--- sitio/src/controller/mainController.js
+++ sitio/src/controller/mainController.js
@@ -1,9 +1,15 @@
-const {render} = require('ejs')
+
 const path = require('path')
+const fs = require('fs')
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json')
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'))
 
 const controller = {
     index: (req, res) => {
-        return res.render('index')
+        return res.render('index', {
+            products
+        })
     }
 
 }
