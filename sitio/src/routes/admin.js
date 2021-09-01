47f41e2e3faf33e2c291f3e5f4560491e5824807
Johnnds <jonnhds@gmail.com>
2021-09-01 14:49:36 -0300

    "Arreglos de CRUD en create y edit,configuración de 'Multer', creación de categorias.json"

--- sitio/src/routes/admin.js
+++ sitio/src/routes/admin.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path= require('path')
 const {create, edit, store,update} =  require('../controller/adminController');
+
+
+const storage = multer.diskStorage({
+    destination: (req,file,callback) => {
+        callback(null,'./public/images')
+    },
+    filename: (req,file,callback) => {
+        callback(null,'img-mate-' + Date.now() + path.extname(file.originalname))
+    },
+});
+
+const upload = multer({
+    storage
+})
+
 
 
 /* create */
 router.get('/create', create);
-router.post('/create', store);
+router.post('/create',upload.single('img'), store);
 
-/* edi*/ 
-router.get('/edit',edit);
-router.put('/edit/:id',update);
+/* edit*/ 
+router.get('/edit/:id', edit);
+router.put('/edit/:id', update);
 
 
 
